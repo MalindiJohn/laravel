baac3f5df5b6b1379bf4e543c937bb26da8fd18f
John Wambua <55756113+w01f-9an9-k@users.noreply.github.com>
2023-03-08 11:29:58 +0300

    Update server.php

--- server.php
+++ server.php
@@ -1,21 +1,13 @@
-<?php
-
-/**
- * Laravel - A PHP Framework For Web Artisans
- *
- * @package  Laravel
- * @author   Taylor Otwell <taylor@laravel.com>
- */
-
-$uri = urldecode(
-    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
-);
-
-// This file allows us to emulate Apache's "mod_rewrite" functionality from the
-// built-in PHP web server. This provides a convenient way to test a Laravel
-// application without having installed a "real" web server software here.
-if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
-    return false;
-}
-
-require_once __DIR__.'/public/index.php';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP project</title>
+</head>
+<body>
+    <h1>This is php project</h1>
+    <p>If you are here, then it means everything is working well</p>
+</body>
+</html>
